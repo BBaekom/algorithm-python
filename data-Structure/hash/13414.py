4c7a4fe704dd80a37c1efb77017e36a55799a5b6
Lee Jongmin <ljm0813b@gmail.com>
2025-02-10 21:05:14 +0900

    13414 solveed

---
+++ data-Structure/hash/13414.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+K, L = map(int, input().split())
+
+st_num = dict()
+lst = []
+
+for i in range(L):
+    tmp = input().rstrip()
+    st_num[tmp] = i
+
+st_num = sorted(st_num.items(), key=lambda x:x[1])
+
+if K > len(st_num):
+    N = len(st_num)
+else:
+    N = K
+
+for i in range(N):
+    print(st_num[i][0])
