2c523fc4587803b13d2daf598cd53f7aaa4e2c34
Lee Jongmin <ljm0813b@gmail.com>
2025-02-15 16:45:24 +0900

    10825 solved

---
+++ sort/10825.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+students = [input().split() for _ in range(N)]
+
+students = sorted(students, key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
+
+for i in students:
+    print(i[0])
